8f29d8c1c0b541dbc061ed5708843cbe0b1af149
Alfan Nur Fauzan <alpancs@gmail.com>
2021-08-11 22:23:42 +0700

    refactor

--- content_builder.go
+++ content_builder.go
@@ -9,13 +9,19 @@ import (
 )
 
 type contentBuilder struct {
-	rb     responseBuilder
-	output *strings.Builder
+	rb              responseBuilder
+	output          *strings.Builder
+	syntax          string
+	forJSONEncoding bool
 }
 
-func newContentBuilder(req responseBuilder) contentBuilder {
-	builder := contentBuilder{req, new(strings.Builder)}
-	return builder
+func newContentBuilder(rb responseBuilder) contentBuilder {
+	syntax := "proto2"
+	if strings.Contains(rb.request.GetParameter(), "syntax=proto3") {
+		syntax = "proto3"
+	}
+	forJSONEncoding := strings.Contains(rb.request.GetParameter(), "encoding=json")
+	return contentBuilder{rb, new(strings.Builder), syntax, forJSONEncoding}
 }
 
 func (b contentBuilder) build(protoFile *descriptorpb.FileDescriptorProto) (string, error) {
@@ -27,16 +33,9 @@ func (b contentBuilder) build(protoFile *descriptorpb.FileDescriptorProto) (stri
 		return "", errors.New(protoFile.GetName() + ": only one top-level type may be defined in a file. use nested types or use imports. see https://developers.google.com/protocol-buffers/docs/proto3 for details.")
 	}
 
-	b.output.WriteString(`syntax = "` + b.getOutputSyntax() + `";` + "\n\n")
+	b.output.WriteString(`syntax = "` + b.syntax + `";` + "\n\n")
 	b.buildMessage(protoFile.GetMessageType()[0], 0)
 	return b.output.String(), nil
-}
-
-func (b contentBuilder) getOutputSyntax() string {
-	if strings.Contains(b.rb.request.GetParameter(), "syntax=proto3") {
-		return "proto3"
-	}
-	return "proto2"
 }
 
 func (b contentBuilder) buildMessage(message *descriptorpb.DescriptorProto, level int) {
@@ -58,8 +57,10 @@ func (b contentBuilder) buildField(field *descriptorpb.FieldDescriptorProto, lev
 }
 
 func (b contentBuilder) buildFieldType(typeName string, level int) string {
-	if typeName, ok := wktMapping[typeName]; ok && b.hasJSONEncoding() {
-		return typeName
+	if b.forJSONEncoding {
+		if typeName, ok := wktMapping[typeName]; ok {
+			return typeName
+		}
 	}
 
 	b.output.WriteString("\n")
@@ -68,14 +69,10 @@ func (b contentBuilder) buildFieldType(typeName string, level int) string {
 	return typeName[strings.LastIndexByte(typeName, '.')+1:]
 }
 
-func (b contentBuilder) hasJSONEncoding() bool {
-	return strings.Contains(b.rb.request.GetParameter(), "encoding=json")
-}
-
 func (b contentBuilder) buildFieldLabel(label descriptorpb.FieldDescriptorProto_Label) {
 	if label == descriptorpb.FieldDescriptorProto_LABEL_REPEATED {
 		b.output.WriteString("repeated ")
-	} else if b.getOutputSyntax() == "proto2" {
+	} else if b.syntax == "proto2" {
 		b.output.WriteString(strings.ToLower(strings.TrimPrefix(label.String(), "LABEL_")) + " ")
 	}
 }
