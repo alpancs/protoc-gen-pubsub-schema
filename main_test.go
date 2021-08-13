8d9db69674cdc29c9888b6da5173160258899f82
Alfan Nur Fauzan <alpancs@gmail.com>
2021-08-14 01:37:30 +0700

    refine the test

--- main_test.go
+++ main_test.go
@@ -5,18 +5,12 @@ import (
 	"io"
 	"os"
 	"testing"
+
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/types/pluginpb"
 )
 
 func Test_process(t *testing.T) {
-	userAddCommentFile, err := os.Open("test/user_add_comment.protobuf")
-	if err != nil {
-		panic(err)
-	}
-	userAddCommentOutput, err := os.ReadFile("test/user_add_comment.pubsub.protobuf")
-	if err != nil {
-		panic(err)
-	}
-
 	type args struct {
 		input io.Reader
 	}
@@ -24,20 +18,54 @@ func Test_process(t *testing.T) {
 		name       string
 		args       args
 		wantOutput []byte
+		wantErr    bool
 	}{
 		{
 			name:       "sanity check",
-			args:       args{userAddCommentFile},
-			wantOutput: userAddCommentOutput,
+			args:       args{mustOpen("test/user_add_comment.in")},
+			wantOutput: mustRead("test/user_add_comment.out"),
+			wantErr:    false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			output := &bytes.Buffer{}
-			process(tt.args.input, output)
-			if gotOutput := output.Bytes(); !bytes.Equal(gotOutput, tt.wantOutput) {
-				t.Errorf("process() = len=%v, want len=%v", len(gotOutput), len(tt.wantOutput))
+			if err := process(tt.args.input, output); (err != nil) != tt.wantErr {
+				t.Errorf("process() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if gotOutput := output.Bytes(); len(gotOutput) != len(tt.wantOutput) {
+				t.Errorf("process() = %v\nwant %v", mustDecodeResponse(gotOutput), mustDecodeResponse(tt.wantOutput))
 			}
 		})
+
+		if closer, ok := tt.args.input.(io.Closer); ok {
+			closer.Close()
+		}
 	}
+}
+
+func mustOpen(path string) io.Reader {
+	file, err := os.Open(path)
+	if err != nil {
+		panic(err)
+	}
+	return file
+}
+
+func mustRead(path string) []byte {
+	content, err := os.ReadFile(path)
+	if err != nil {
+		panic(err)
+	}
+	return content
+}
+
+func mustDecodeResponse(raw []byte) *pluginpb.CodeGeneratorResponse {
+	resp := new(pluginpb.CodeGeneratorResponse)
+	err := proto.Unmarshal(raw, resp)
+	if err != nil {
+		panic(err)
+	}
+	return resp
 }
